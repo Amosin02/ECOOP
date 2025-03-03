148974f96a77464285c7c9492f85c8a657ccefc2
Nhoj Amosin <amosinnhoj@gmail.com>
2025-03-03 20:53:02 +0800

    added a delete button and visible state change when approving/rejecting and in deletion

--- frontend/src/components/FormNumber.jsx
+++ frontend/src/components/FormNumber.jsx
@@ -4,15 +4,46 @@ import FormPage from './FormPage';
 
 export default function FormNumber() {
   const [loanData, setLoanData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('http://localhost:5001/api/entries');
-      const result = await response.data;
-      setLoanData(result.entries);
-    };
+  const fetchData = async () => {
+    const response = await axios.get('http://localhost:5001/api/entries');
+    const result = await response.data;
+    setLoanData(result.entries);
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
 
-  return <FormPage loanData={loanData} />;
+  async function handleAction(loanId, value) {
+    const changedValue = value ? 'approved' : 'rejected';
+    const id = loanId;
+
+    const changeAction = { action: changedValue };
+
+    try {
+      await axios.put(`http://localhost:5001/api/entries/${id}`, changeAction);
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleDelete(loanId) {
+    const id = loanId;
+
+    try {
+      await axios.delete(`http://localhost:5001/api/entries/${id}`);
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <FormPage
+      loanData={loanData}
+      handleAction={handleAction}
+      handleDelete={handleDelete}
+    />
+  );
 }
--- frontend/src/components/FormPage.jsx
+++ frontend/src/components/FormPage.jsx
@@ -1,10 +1,13 @@
 import { Button, Table } from '@chakra-ui/react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { IoCheckmarkSharp } from 'react-icons/io5';
 import { RxCross2 } from 'react-icons/rx';
+import { FaRegTrashAlt } from 'react-icons/fa';
 
 export default function FormPage(props) {
   const loans = props.loanData;
+
   function currencyFormat(num) {
     return parseInt(num)
       .toFixed()
@@ -16,63 +19,77 @@ export default function FormPage(props) {
     return capitalize;
   }
 
-  async function handleAction(loanId, value) {
-    const changedValue = value ? 'approved' : 'rejected';
-    const id = loanId;
-
-    const changeAction = { action: changedValue };
-
-    try {
-      await axios.put(`http://localhost:5001/api/entries/${id}`, changeAction);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
-    <Table.Root stickyHeader width={'70%'}>
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader>Name</Table.ColumnHeader>
-          <Table.ColumnHeader>Amount</Table.ColumnHeader>
-          <Table.ColumnHeader>C/O</Table.ColumnHeader>
-          <Table.ColumnHeader>Request</Table.ColumnHeader>
-          <Table.ColumnHeader>Action</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-
-      <Table.Body>
-        {loans.map((value) => (
-          <Table.Row key={value._id}>
-            <Table.Cell>{value.fullName}</Table.Cell>
-            <Table.Cell>{currencyFormat(value.amount)}</Table.Cell>
-            <Table.Cell>{value.co}</Table.Cell>
-            <Table.Cell>{requestFormat(value.action)}</Table.Cell>
-            <Table.Cell>
-              {value.action === 'pending' && (
-                <>
-                  <Button
-                    bg={'green.600'}
-                    size={'xs'}
-                    width={5}
-                    rounded={'full'}
-                    onClick={() => handleAction(value._id, 1)}>
-                    <IoCheckmarkSharp />
-                  </Button>
-                  <Button
-                    bgColor={'red'}
-                    size={'xs'}
-                    width={5}
-                    rounded={'full'}
-                    onClick={() => handleAction(value._id, 0)}>
-                    <RxCross2 />
-                  </Button>
-                </>
-              )}
-            </Table.Cell>
+    <>
+      <h1>Total Loans: {props.loanData.length}</h1>
+      <Table.Root stickyHeader width={'70%'}>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>Name</Table.ColumnHeader>
+            <Table.ColumnHeader>Amount</Table.ColumnHeader>
+            <Table.ColumnHeader>C/O</Table.ColumnHeader>
+            <Table.ColumnHeader>Request</Table.ColumnHeader>
+            <Table.ColumnHeader>Action</Table.ColumnHeader>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+
+        <Table.Body>
+          {loans.map((value) => (
+            <Table.Row key={value._id}>
+              <Table.Cell>{value.fullName}</Table.Cell>
+              <Table.Cell>{currencyFormat(value.amount)}</Table.Cell>
+              <Table.Cell>{value.co}</Table.Cell>
+              {value.action === 'rejected' && (
+                <Table.Cell color={'red'} fontWeight={'bolder'}>
+                  {requestFormat(value.action)}
+                </Table.Cell>
+              )}
+              {value.action === 'approved' && (
+                <Table.Cell color={'green.600'} fontWeight={'bolder'}>
+                  {requestFormat(value.action)}
+                </Table.Cell>
+              )}
+              {value.action === 'pending' && (
+                <Table.Cell>{requestFormat(value.action)}</Table.Cell>
+              )}
+              <Table.Cell>
+                {value.action === 'pending' && (
+                  <>
+                    <Button
+                      bg={'green.600'}
+                      size={'xs'}
+                      width={5}
+                      rounded={'full'}
+                      onClick={() => props.handleAction(value._id, 1)}>
+                      <IoCheckmarkSharp />
+                    </Button>
+                    <Button
+                      bgColor={'red'}
+                      size={'xs'}
+                      width={5}
+                      rounded={'full'}
+                      onClick={() => props.handleAction(value._id, 0)}>
+                      <RxCross2 />
+                    </Button>
+                  </>
+                )}
+                {value.action == 'rejected' && (
+                  <>
+                    <Button
+                      bgColor={'red'}
+                      size={'xs'}
+                      width={5}
+                      rounded={'full'}
+                      onClick={() => props.handleDelete(value._id)}>
+                      <FaRegTrashAlt />
+                    </Button>
+                  </>
+                )}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 }
